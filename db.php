f3a0a06c5fbacebccd19fa307f52e1e7cad4b8a1
kaouther <kaoutheeramdouni.com>
2025-10-09 15:30:04 +0100

    modif name of databese

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $host    = getenv('DB_HOST') ?: 'mysql-service';
-$db      = getenv('DB_NAME') ?: 'site5g';
+$db      = getenv('DB_NAME') ?: 'site5gdb';
 $user    = getenv('DB_USER') ?: 'site5guser';
 $pass    = getenv('DB_PASSWORD') ?: 'Site5gPass123';
 $charset = 'utf8mb4';
